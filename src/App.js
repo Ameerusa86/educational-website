cc01e9ef279bac69c14f6df1c1073ec44733aa48
Ameerusa86 <engamermecha@gmail.com>
2023-04-01 10:26:42 -0400

    Creating the Navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Components/Common/Heading/Header";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
